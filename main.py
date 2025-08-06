8e662c79dc7d9b3884726ab225581eb45d7c8b1f
MDT RIVINHARA <40-bit-0006@kdu.ac.lk>
2025-08-06 13:12:10 +0530

    Hello world

---
+++ main.py
@@ -0,0 +1,43 @@
+# print("hello thirandi")
+# print("Welcome to phython")
+# print(7+3)
+# print(type(6.66))
+# print(10%2==0)
+# print("AI"+"Chatbot")
+
+
+# i=7
+# if(i<15):
+#     print("i is smaller than 15")
+#     print("i'm in if Block")
+# else:
+#     print("i is greater than 15")
+#     print("i'm in else Block")
+#     print("i'm not in if and not in else block")
+
+# i=20
+# if(i==10):
+#     print("i is 10")
+# elif(i==15):
+#     print("i is 15")
+# elif(i==20):
+#     print("i is 20")
+# else:
+#     print("i is not present")        
+
+# i=1
+# while i<6:
+#   print(i)
+#   i+=1
+
+# fruits=["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+
+# marks=input("Enter your marks: ")
+# print(type(marks))
+mark1 = int(input("Enter first mark: "))
+mark2 = int(input("Enter second mark: "))
+total = mark1 + mark2
+print("Total marks:", total)
+        
